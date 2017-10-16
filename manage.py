baa67f50f50922cd72c24fcad1b7a16a030f52fe
smallstrong0 <393019766@qq.com>
2017-10-17 00:00:14 +0800

    defind the 400+ page

--- manage.py
+++ manage.py
@@ -1,20 +1,28 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, abort, render_template_string
 
 app = Flask(__name__)
 
-apis = {
+whale_apis = {
     'test'
 }
 
 
 @app.route('/api/<func>', methods=['GET', 'POST'])
-def hello_world(func):
-    exec 'import ' + func
-    data = eval(func + '.go()')
-    return data
+def go(func):
+    if func not in whale_apis:
+        return abort(401)
+    else:
+        exec 'import fe.' + func
+        data = eval('fe.' + func + '.go()')
+        return data
+
+
+@app.errorhandler(401)
+def page_unauthorized(error):
+    return render_template_string('<h1>{{ error_info }}</h1>', error_info=error), 401
 
 
 if __name__ == '__main__':
