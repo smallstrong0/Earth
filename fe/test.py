92e0048e6e1f4e81771c2c4e7563c70fb50a4d63
smallstrong0 <393019766@qq.com>
2017-10-17 00:00:25 +0800

    test api success

--- fe/test.py
+++ fe/test.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import lib.common.func as cn_func
 
 
 def go():
-    print 'auto publish'
-    return 'intersting'
+    keys = {
+        'ts': None,
+    }
+
+    params, error = cn_func.get_web_params(keys)
+    return cn_func.check_sort_serialize(params)
