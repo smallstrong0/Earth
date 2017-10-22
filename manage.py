6f35840517a06dfad0133ffea86a1fab0deca69e
smallstrong0 <393019766@qq.com>
2017-10-23 00:05:15 +0800

    remove 401

--- manage.py
+++ manage.py
@@ -12,17 +12,20 @@ whale_apis = {
 
 @app.route('/api/<func>', methods=['GET', 'POST'])
 def go(func):
-    if func not in whale_apis:
-        return abort(401)
-    else:
-        exec 'import fe.' + func
-        data = eval('fe.' + func + '.go()')
-        return data
+    # if func not in whale_apis:
+    #     return abort(401)
+    # else:
+    #     exec 'import fe.' + func
+    #     data = eval('fe.' + func + '.go()')
+    #     return data
+    exec 'import fe.' + func
+    data = eval('fe.' + func + '.go()')
+    return data
 
 
-@app.errorhandler(401)
-def page_unauthorized(error):
-    return render_template_string('<h1>{{ error_info }}</h1>', error_info=error), 401
+# @app.errorhandler(401)
+# def page_unauthorized(error):
+#     return render_template_string('<h1>{{ error_info }}</h1>', error_info=error), 401
 
 
 if __name__ == '__main__':
