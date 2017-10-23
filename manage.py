5dd54fbd172afbe252d935c45bda997427c9025d
smallstrong0 <393019766@qq.com>
2017-10-23 19:30:36 +0800

    add spider package

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ whale_apis = {
 }
 
 
-@app.route('/<func>', methods=['GET', 'POST'])
+@app.route('/api/<func>', methods=['GET', 'POST'])
 def go(func):
     # if func not in whale_apis:
     #     return abort(401)
