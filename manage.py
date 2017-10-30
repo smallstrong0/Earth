0eec052b9e4b3cdf946357b3d759266f25746fb9
smallstrong0 <393019766@qq.com>
2017-10-30 15:46:05 +0800

    add mongodb core file

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, abort, render_template_string
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -12,21 +12,11 @@ whale_apis = {
 
 @app.route('/api/<func>', methods=['GET', 'POST'])
 def go(func):
-    # if func not in whale_apis:
-    #     return abort(401)
-    # else:
-    #     exec 'import fe.' + func
-    #     data = eval('fe.' + func + '.go()')
-    #     return data
     exec 'import fe.' + func
     data = eval('fe.' + func + '.go()')
     return data
 
 
-# @app.errorhandler(401)
-# def page_unauthorized(error):
-#     return render_template_string('<h1>{{ error_info }}</h1>', error_info=error), 401
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+
