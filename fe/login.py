8f7dfc566d8d143b155794aacf694a7cf7cee242
smallstrong0 <393019766@qq.com>
2017-10-30 20:35:44 +0800

    test login

--- fe/login.py
+++ fe/login.py
@@ -7,15 +7,18 @@ import tool.c_utils as c_tool
 
 def go():
     keys = {
-        'ts': None,
-        'nonce': None,
-        'test': None,
+        'wechat_id': None,
+
     }
-    error, params = check.go(keys)
+    error, params = check.simple_go(keys)
+    if error is None:
+        dic = {
+            'user_id': 1
+        }
 
     if error is None:
         print c_tool.check_sort_serialize(data=params)
         return c_tool.check_sort_serialize(data=params)
     else:
-        print c_tool.check_sort_serialize(msg='error')
-        return c_tool.check_sort_serialize(msg='error')
+        print c_tool.check_sort_serialize(msg=error)
+        return c_tool.check_sort_serialize(msg=error)
