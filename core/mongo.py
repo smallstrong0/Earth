2ca22c9047dca3a96b5e617e559360ff8fea028a
smallstrong0 <393019766@qq.com>
2017-10-31 09:54:38 +0800

    core mongo just return the db

--- core/mongo.py
+++ core/mongo.py
@@ -1,19 +1,6 @@
-import pymongo
+from pymongo import MongoClient
 
 
 class DbUtils(object):
-    def __init__(self, db_name='power', collection_name=''):
-        exec ("self.db = pymongo.MongoClient(host='localhost', port=27017)." + db_name)
-        exec ("self.collection = self.db." + collection_name)
-
-    def add(self, data=[]):
-        return True if self.collection.insert_many(data) else False
-
-    def update(self, old={}, new={}):
-        return True if self.collection.update(old, {'$set': new}) else False
-
-    def select(self, where={}, field=[], limits=3, ordering=[]):
-        return self.collection.find(where, field).limit(limits).sort(ordering)
-
-    def delete(self, field={}):
-        return True if self.collection.remove(field) else False
+    def __init__(self):
+        self.db = MongoClient(host='localhost', port=27017).power
