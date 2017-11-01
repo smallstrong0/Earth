9da94832269257fb2be609fa395c808cc9ffddb3
smallstrong0 <393019766@qq.com>
2017-11-01 21:07:35 +0800

    dap of mod_user_info

---
+++ dap/mod_user_info.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import tool.c_utils as c_tool
+import tool.t_utils as t_tool
+import dao.user as dao_user
+import pymongo
+
+
+def set_user_info(params):
+    result = dao_user.select({'user_id': params['user_id']}, ['wechat_id', 'user_id'], 1, [('port', pymongo.ASCENDING)])
+    if result.count() > 0:
+
+        pass
