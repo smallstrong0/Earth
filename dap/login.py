4ae83b0ea96cb6e5e5c0f6465b50dacb85abbb37
smallstrong0 <393019766@qq.com>
2017-11-01 21:07:02 +0800

    just change name

--- dap/login.py
+++ dap/login.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 import tool.c_utils as c_tool
 import tool.t_utils as t_tool
-import dao.login as dao_login
+import dao.user as dao_user
 import pymongo
 
 
 def get_wx_user_id(openid):
-    result = dao_login.select({'wechat_id': openid}, ['wechat_id', 'user_id'], 1, [('port', pymongo.ASCENDING)])
+    result = dao_user.select({'wechat_id': openid}, ['wechat_id', 'user_id'], 1, [('port', pymongo.ASCENDING)])
     if result.count() > 0:
         return result[0]['user_id']
     else:
-        data = dao_login.create(
+        data = dao_user.create(
             {'wechat_id': openid, 'user_id': c_tool.guid(), 'ctime': t_tool.get_ts(), 'mtime': t_tool.get_ts()})
         if data:
             return data['user_id']
