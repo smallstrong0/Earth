1f5ba8dec4dd9f8f5694a704d1b4f6e0b97e05ab
smallstrong0 <393019766@qq.com>
2017-11-01 21:07:25 +0800

    api of mod_user_info

---
+++ fe/mod_user_info.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import core.check as check
+import tool.c_utils as c_tool
+import requests
+import dap.mod_user_info as d_mod_user_info
+
+
+def go():
+    keys = {
+        'user': None,
+        'user_info': None,
+    }
+    error, params = check.simple_go(keys)
+
+    dic = {
+        'stastus': 'ok',
+    }
+
+    if error is None:
+        print c_tool.sort_serialize(params)
+        # d_mod_user_info.set_user_info(params)
+
+    if error is None:
+        return c_tool.check_sort_serialize(data=dic)
+    else:
+        return c_tool.check_sort_serialize(msg=error)
