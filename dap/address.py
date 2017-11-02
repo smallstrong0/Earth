8b9a4ce761d997ffa6e974f5e4fb193f1dac687c
smallstrong0 <393019766@qq.com>
2017-11-02 22:43:26 +0800

    dap of address

---
+++ dap/address.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import tool.c_utils as c_tool
+import tool.t_utils as t_tool
+import dao.address as dao_address
+import pymongo
+
+
+def add_address(params):
+    params['address'] = c_tool.guid()
+    return dao_address.create(params)
+
+
+def mod_address(params):
+    return dao_address.update({'user_id': params['user_id'], 'address_id': params['address_id']},
+                              params)
+
+
+def remove_address(params):
+    return dao_address.delete(params)
+
+
+def get_address_list(params):
+    return dao_address.select({'user_id': params['user_id']}, [], 10, [('port', pymongo.ASCENDING)])
