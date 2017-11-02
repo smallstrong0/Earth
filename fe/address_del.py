5279c7c9fd1b8f0103972c9b4299c55c27060b46
smallstrong0 <393019766@qq.com>
2017-11-02 22:42:11 +0800

    del address

---
+++ fe/address_del.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import core.check as check
+import tool.c_utils as c_tool
+import dap.address as d_address
+
+
+def go():
+    keys = {
+        'user_id': None,
+        'address_id': None,
+    }
+    error, params = check.simple_go(keys)
+
+    dic = {
+    }
+
+    if error is None:
+        result = d_address.remove_address(params)
+        if result:
+            dic.update(params)
+        else:
+            error = '删除地址失败'
+
+    if error is None:
+        return c_tool.check_sort_serialize(data=dic)
+    else:
+        return c_tool.check_sort_serialize(msg=error)
