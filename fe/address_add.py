f6b5a8940b1002340a034a56e163b689496f5ad5
smallstrong0 <393019766@qq.com>
2017-11-02 22:42:47 +0800

    add address

---
+++ fe/address_add.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import core.check as check
+import tool.c_utils as c_tool
+import dap.address as d_address
+
+
+def go():
+    keys = {
+        'user_id': None,
+        'name': None,
+        'phone': None,
+        'qq': None,
+        'we_chat': '',
+        'address': None,
+        'place': None,
+    }
+    error, params = check.simple_go(keys)
+
+    dic = {
+    }
+
+    if error is None:
+        result = d_address.add_address(params)
+        if result:
+            dic.update(result)
+        else:
+            error = '添加地址失败'
+
+    if error is None:
+        return c_tool.check_sort_serialize(data=dic)
+    else:
+        return c_tool.check_sort_serialize(msg=error)
