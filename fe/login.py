70eca4f7aa7321979e466dc079f868949f758b1c
smallstrong0 <393019766@qq.com>
2017-11-06 09:30:31 +0800

    【API】provide the time_stamps method

--- fe/login.py
+++ fe/login.py
@@ -3,6 +3,7 @@
 
 import core.check as check
 import tool.c_utils as c_tool
+import tool.t_utils as t_tool
 import requests
 import dap.login as d_login
 
@@ -17,7 +18,8 @@ def go():
     dic = {
         'openid': '',
         'session_key': '',
-        'user_id': ''
+        'user_id': '',
+        'time_stamps_zh': []
     }
 
     if error is None:
@@ -27,8 +29,18 @@ def go():
         dic['openid'] = r.json()['openid']
         dic['session_key'] = r.json()['session_key']
         dic['user_id'] = d_login.get_wx_user_id(dic['openid'])
+        dic['time_stamps_zh'] = get_time_stamps()
 
     if error is None:
         return c_tool.check_sort_serialize(data=dic)
     else:
         return c_tool.check_sort_serialize(msg=error)
+
+
+def get_time_stamps():
+    z_ts = t_tool.get_today_zero_time()
+    first_stamp = z_ts + 3600 * 9
+    time_stamps_zh = []
+    for i in 7:
+        time_stamps_zh.append(t_tool.get_time(first_stamp + 3600 * i))
+    return time_stamps_zh
