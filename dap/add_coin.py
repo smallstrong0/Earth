134f1ed37a0e0e95644fb8460e451dc043060883
smallstrong0 <393019766@qq.com>
2017-12-09 22:29:48 +0800

    【Dap】upper sign

--- dap/add_coin.py
+++ dap/add_coin.py
@@ -25,7 +25,7 @@ def get_pay_sign(prepay_id):
     pay_sign = pay_sign + 'key={}'.format(key.store_key)
 
     print pay_sign
-    return c_tool.md5(pay_sign)
+    return str(c_tool.md5(pay_sign)).upper()
 
 
 def get_data(params):
