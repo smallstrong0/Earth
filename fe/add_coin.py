2c1f39b9d99928eb21944343bd23ce7f0a766563
smallstrong0 <393019766@qq.com>
2017-12-09 21:00:53 +0800

    【API】add api of add_coin

---
+++ fe/add_coin.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import core.check as check
+import tool.c_utils as c_tool
+import requests
+import dap.add_coin
+import key
+
+
+def go():
+    keys = {
+        'user_id': None,
+        'money': None,
+        'openid': None
+    }
+    error, params = check.simple_go(keys)
+
+    dic = {
+    }
+
+    if error is None:
+        data = dap.add_coin.get_data(params)
+        r = requests.post(
+            'https://api.mch.weixin.qq.com/pay/unifiedorder', data)
+        print r
+
+    if error is None:
+        return c_tool.check_sort_serialize(data=dic)
+    else:
+        return c_tool.check_sort_serialize(msg=error)
